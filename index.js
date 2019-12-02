58a91b29a2e8254dd2ca06d8adf95d34619ccdbe
uthdev <adelekegbolahan92@yahoo.com>
2019-12-02 12:36:09 +0100

    chore(setup): project structure setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
